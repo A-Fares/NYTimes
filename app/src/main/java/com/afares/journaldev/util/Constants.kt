dc6a50f33c7db088387fa245f712adbceb3c405f
AFares1 <ahmedfaresa10@gmail.com>
2021-03-27 20:36:54 +0200

    init commit setup app

---
+++ app/src/main/java/com/afares/journaldev/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.afares.journaldev.util
+
+class Constants {
+    companion object {
+
+        const val BASE_URL = "https://api.nytimes.com/svc/mostpopular/"
+        const val API_KEY = "BtaY0U2i5TPijKSy8Vn1wAbvPktDWWQz"
+
+    }
+}
